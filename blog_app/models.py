92e154d5f3bd2c3099655481cc4e641edd26ddee
deepak-dj <joshig.deepak@gmail.com>
2024-07-18 01:45:29 +0530

    removing comment

--- blog_app/models.py
+++ blog_app/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True, validators=[EmailValidator])
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)  # Add this line
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
 
